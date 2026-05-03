0f4e5f935d3edaa6a2784e9ddb5d5be47ccf4078
Dranov220805 <duongthanhlong220805@gmail.com>
2026-05-03 16:46:46 +0700

    fix: fix error in PR #4

--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const os = require('os');
 const router = express.Router();
 const dataSource = require('../services/dataSource');
+const runtimeInfo = require('../services/runtimeInfo');
 
 router.get('/', async (req, res, next) => {
   try {
     const products = await dataSource.getAll();
-    res.render('index', { products, hostname: require('os').hostname(), source: dataSource.isMongo ? 'mongodb' : 'in-memory' });
+    const runtime = runtimeInfo.getSnapshot({ appUsesMongo: dataSource.isMongo });
+    res.render('index', {
+      products,
+      hostname: os.hostname(),
+      source: dataSource.isMongo ? 'mongodb' : 'in-memory',
+      runtime
+    });
   } catch (err) { next(err); }
 });
 
